bc2b97b5d4e991536b012e808d1f13902e10a6b5
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-11-26 15:58:52 +1300

    Let Game model calculate the state from the actions

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,30 @@
+require_relative '../concepts/game_state'
+require_relative '../concepts/player_state'
+require_relative '../services/apply_action'
+
 class Game < ActiveRecord::Base
   has_many :players
+
+  after_initialize :setup_state
+
+  def actions
+    # TODO: optimize
+    players.all
+           .flat_map { |player| player.actions }
+           .sort_by { |action| action.id }
+  end
+
+  private
+
+  def setup_state
+    @game = Game.new(initial_player_states)
+
+    # Apply the actions
+    actions.each { |action| ApplyAction.new(self).call(action) }
+  end
+
+  def initial_player_states
+    players.all.map { |player| PlayerState.new(player.id) }
+  end
 end
+
