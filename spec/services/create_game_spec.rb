ba1b123f6d3a7b9ca2e75f1ac6762bda83b632fe
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-11-27 16:22:00 +1300

    Added CreateGame tests

--- spec/services/create_game_spec.rb
+++ spec/services/create_game_spec.rb
@@ -2,4 +2,11 @@ require_relative '../../app/services/create_game'
 
 describe CreateGame do
 
+  it "creates a game object" do
+    expect(CreateGame.new.call(2)).to be_a Game
+  end
+
+  it "creates four players when you ask it to" do
+    expect(CreateGame.new.call(4).players.size).to eq 4
+  end
 end
