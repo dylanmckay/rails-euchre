1681712d5b904bf5fef6d4fb31174aaee7d22c70
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-12-03 12:09:52 +1300

    Really bad commit name

--- app/assets/javascripts/games.js
+++ app/assets/javascripts/games.js
@@ -1,5 +1,110 @@
 
+function Card(suit, rank) {
+  this.suit = suit;
+  this.rank = rank;
+
+  this.getHTMLId = function() {
+    return this.suit + "_" + this.rank;
+  };
+
+  this.getElement = function() {
+    return $( "#" + this.getHTMLId() );
+  };
+
+  return this;
+}
+
+function removeNode(node) {
+  node.remove();
+}
+
+function getPileElement() {
+  $( "#pile ");
+}
+
+function removeCardFromHand(card) {
+  removeNode(card.getElement());
+}
+
+function addCardToPile(card) {
+  pile.innerHTML = pile.innerHTML + getSymbolForCard(card);
+}
+
+function getSymbolForCard(card) {
+  switch (card.suit) {
+  case "spades":
+    switch (card.rank) {
+      case 1: return "🂡";
+      case 2: return "🂢";
+      case 3: return "🂣";
+      case 4: return "🂤";
+      case 5: return "🂥";
+      case 6: return "🂦";
+      case 7: return "🂧";
+      case 8: return "🂨";
+      case 9: return "🂩";
+      case 10: return "🂪";
+      case 11: return "🂫";
+      case 12: return "🂭";
+      case 13: return "🂮";
+    }
+    break;
+  case "hearts":
+    switch (card.rank) {
+      case 1: return "🂱";
+      case 2: return "🂲";
+      case 3: return "🂳";
+      case 4: return "🂴";
+      case 5: return "🂵";
+      case 6: return "🂶";
+      case 7: return "🂷";
+      case 8: return "🂸";
+      case 9: return "🂹";
+      case 10: return "🂺";
+      case 11:return "🂻";
+      case 12:return "🂽";
+      case 13:return "🂾";
+    }
+    break;
+  case "diamonds":
+    switch (card.rank) {
+      case 1: return "🃁";
+      case 2: return "🃂";
+      case 3: return "🃃";
+      case 4: return "🃄";
+      case 5: return "🃅";
+      case 6: return "🃆";
+      case 7: return "🃇";
+      case 8: return "🃈";
+      case 9: return "🃉";
+      case 10: return "🃊";
+      case 11:return "🃋";
+      case 12:return "🃍";
+      case 13:return "🃎";
+    }
+    break;
+  case "clubs":
+    switch (card.rank) {
+      case 1: return "🃑";
+      case 2: return "🃒";
+      case 3: return "🃓";
+      case 4: return "🃔";
+      case 5: return "🃕";
+      case 6: return "🃖";
+      case 7: return "🃗";
+      case 8: return "🃘";
+      case 9: return "🃙";
+      case 10: return "🃚";
+      case 11:return "🃛";
+      case 12:return "🃝";
+      case 13:return "🃞";
+    }
+  }
+}
+
 function play_card(game, player, suit, rank) {
+  card = Card(suit, rank);
+
   $.ajax({
     type: "POST",
     url: "/games/" + game + "/players/" + player + "/operations",
@@ -12,5 +117,8 @@ function play_card(game, player, suit, rank) {
       },
     },
   });
-  location.reload();
+
+  removeCardFromHand(card);
+  addCardToPile(card);
 }
+
--- app/helpers/games_helper.rb
+++ app/helpers/games_helper.rb
@@ -1,2 +1,12 @@
 module GamesHelper
+  def card_onclick_handler(card)
+    "play_card(#{@game.id},
+               #{@game_state.players[0].id},
+               \"#{card.suit}\",
+               #{card.rank})"
+  end
+
+  def card_html_id(card)
+    "#{card.suit}_#{card.rank}"
+  end
 end
