9aea237a2c2cd5eb816e7adf3c5abb6f40fcb114
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-12-02 16:18:37 +1300

    Removed print debugging statements

--- app/controllers/operations_controller.rb
+++ app/controllers/operations_controller.rb
@@ -1,16 +1,12 @@
 class OperationsController < ApplicationController
   def create
     operation = Operation.create!(create_params)
-    puts "SOMETHING #{create_params}"
-    puts "HERE #{operation} AND ALSO #{operation.inspect}"
     p = Player.find(operation.player_id)
-    puts "HERE IS PLAYER #{p}"
     redirect_to Player.last.game
   end
 
   def show
     operation = Operation.find(params[:operation_id])
-    puts "THINGY #{operation.player}"
     redirect_to operation.player
   end
 
