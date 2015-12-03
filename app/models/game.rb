4cb3a496d6b94aa24a5c60eb86d946612c82925c
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-12-03 14:19:56 +1300

    Remove game state from game model

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,20 +2,11 @@ class Game < ActiveRecord::Base
   has_many :players
   #has_many :operations, through: :players
 
-  before_create :setup_state
-
   # def operations
   #   Operation.all.select { |op| op.player.game.id == id }.sort
   # end
 
   private
-
-  def setup_state
-    @game = GameState.new(initial_player_states)
-
-    # Apply the operations
-    operations.each { |operation| ApplyOperation.new(self).call(operation) }
-  end
 
   def initial_player_states
     players.all.map { |player| PlayerState.new(player.id) }
