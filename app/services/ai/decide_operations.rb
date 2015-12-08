1fb4d540f5d29bcd66ca0a3f3940d8ac49e85982
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-12-08 16:05:28 +1300

    Cleaned up 'AI::DecideOperations'

--- app/services/ai/decide_operations.rb
+++ app/services/ai/decide_operations.rb
@@ -7,43 +7,20 @@ module AI
     end
 
     def call
-      if last_operation && last_operation.play_card?
-        #make an operation based on the computer's decision
-        ai_operations
-      else
-        []
-      end
+      ai_players.map { |ai| decide_operation(ai) }
     end
 
     private
 
-    def player_operation?(operation)
-      operation.player_id == @game.players.first.id
+    def ai_players
+      @game.players[1..-1]
     end
 
-    def last_operation
-      @game.operations.last
-    end
+    def decide_operation(ai)
+      ai_state = @game_state.find_player(ai.id)
+      card = AI::DecidePlay.new(ai_state, @game_state).call
 
-    def ai_operations
-      if player_operation?(last_operation)
-        create_ai_operation_set
-      else
-        []
-      end
-    end
-
-    def create_ai_operation_set
-      @game.players[1..-1].map do |ai|
-        create_ai_operation(ai)
-      end
-    end
-
-    def create_ai_operation(player)
-      player_state = @game_state.find_player(player.id)
-      card = AI::DecidePlay.new(player_state, @game_state).call
-
-      player.operations.create!(
+      ai.operations.create!(
         operation_type: "play_card",
         suit: card.suit,
         rank: card.rank
