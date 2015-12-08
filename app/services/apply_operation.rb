587cd6685a4e7915d992080a99a15eba89507ed4
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-12-08 17:00:52 +1300

    Refactored ApplyOperation

--- app/services/apply_operation.rb
+++ app/services/apply_operation.rb
@@ -3,32 +3,38 @@ class ApplyOperation
   def initialize(game_state, operation)
     @game_state = game_state
     @operation = operation
+    @player = @game_state.find_player(@operation.player_id)
   end
 
   def call
-    player = @game_state.find_player(@operation.player_id)
-
-    if @operation.deal_card?
-      player.hand << @operation.card
-
-    elsif @operation.pass_trump?
-      @game_state.trump_state.selection_operations << :pass
-
-    elsif @operation.accept_trump?
-      @game_state.trump_state.selection_operations << :accept
-      @game_state.trump_state.suit = @game_state.trump_state.selection_suit
-
-    elsif @operation.pick_trump?
-      @game_state.trump_state.selection_operations << :pick
-      @game_state.trump_state.suit = @operation.suit.to_sym
-
-    elsif @operation.play_card?
-      @game_state.pile.add(player.hand.delete(@operation.card), player)
-      finish_trick if every_player_has_played?
+    case @operation.type
+    when :deal_card    then deal_card
+    when :pass_trump   then pass_trump
+    when :accept_trump then accept_trump
+    when :pick_trump   then pick_trump
+    when :play_card    then play_card
     end
   end
 
   private
+
+  def deal_card
+    @player.hand << @operation.card
+  end
+
+  def pass_trump
+    @game_state.trump_state.selection_operations << :pass
+  end
+
+  def accept_trump
+    @game_state.trump_state.selection_operations << :accept
+    @game_state.trump_state.suit = @game_state.trump_state.selection_suit
+  end
+
+  def pick_trump
+    @game_state.trump_state.selection_operations << :pick
+    @game_state.trump_state.suit = @operation.suit.to_sym
+  end
 
   def finish_trick
     winner = @game_state.trick_winner
@@ -36,6 +42,11 @@ class ApplyOperation
     @game_state.pile.clear
 
     finish_round if every_player_has_no_cards?
+  end
+
+  def play_card
+    @game_state.pile.add(@player.hand.delete(@operation.card), @player)
+    finish_trick if every_player_has_played?
   end
 
   def finish_round
@@ -58,7 +69,6 @@ class ApplyOperation
   end
 
   def current_dealer_index
-    puts "dealer: #{@game_state.dealer.id}"
     @game_state.players.find_index do |player|
       player == @game_state.dealer
     end
