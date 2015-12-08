741cb67d11a618fce30647eae35342ba20b08cf2
Tana Tanoi <tanatanoi@gmail.com>
2015-12-09 12:46:32 +1300

    Reduced amount of queries sent to server per reload

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,9 +2,7 @@ class Game < ActiveRecord::Base
   has_many :players
 
   def operations
-    #FIXME: rails queries rather than ruby sorting
-    #       this is really hurting performance
-    Operation.all.select { |op| op.player.game.id == id }.sort
+    Operation.where(:game == self).sort
   end
 
   def main_player
