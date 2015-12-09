1c27283a5994ccf8472d4c30efe5498b9dd755a1
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-12-09 13:03:01 +1300

    Added scope constructors
    Closes #14.

--- app/models/operation.rb
+++ app/models/operation.rb
@@ -2,7 +2,31 @@ class Operation < ActiveRecord::Base
   belongs_to :player
   has_one :game, through: :player
 
-  scope :ordered, -> { order(:id) }
+  scope :deal_card!, ->(card) {
+    create!(operation_type: "deal_card",
+            suit: card.suit,
+            rank: card.rank)
+  }
+
+  scope :pass_trump!, -> {
+    create!(operation_type: "pass_trump")
+  }
+
+  scope :accept_trump!, -> {
+    create!(operation_type: "accept_trump")
+  }
+
+  scope :pick_trump!, ->(card) {
+    create!(operation_type: "pick_trump",
+            suit: card.suit,
+            rank: card.rank)
+  }
+
+  scope :play_card!, ->(card) {
+    create!(operation_type: "play_card",
+            suit: card.suit,
+            rank: card.rank)
+  }
 
   def deal_card?
     type == :deal_card
--- spec/models/operation_spec.rb
+++ spec/models/operation_spec.rb
@@ -1,12 +1,78 @@
 require 'rails_helper'
 
 describe Operation do
+  let(:card) { Card.new(:hearts, 10) }
+
+  describe "scopes" do
+
+    describe "deal_card!" do
+      subject(:operation) { Operation.deal_card!(card) }
+
+      it { is_expected.to be_an Operation }
+
+      it "has the right card" do
+        expect(operation.card).to eq card
+      end
+
+      it "has the right operation type" do
+        expect(operation.operation_type).to eq "deal_card"
+      end
+    end
+
+    describe "pass_trump!" do
+      subject(:operation) { Operation.pass_trump! }
+
+      it { is_expected.to be_an Operation }
+
+      it "has the right operation type" do
+        expect(operation.operation_type).to eq "pass_trump"
+      end
+    end
+
+    describe "accept_trump!" do
+      subject(:operation) { Operation.accept_trump! }
+
+      it { is_expected.to be_an Operation }
+
+      it "has the right operation type" do
+        expect(operation.operation_type).to eq "accept_trump"
+      end
+    end
+
+    describe "pick_trump!" do
+      subject(:operation) { Operation.pick_trump!(card) }
+
+      it { is_expected.to be_an Operation }
+
+      it "has the right operation type" do
+        expect(operation.operation_type).to eq "pick_trump"
+      end
+
+      it "has the right card" do
+        expect(operation.card).to eq card
+      end
+    end
+  end
+
+  describe "play_card!" do
+    subject(:operation) { Operation.play_card!(card) }
+
+    it { is_expected.to be_an Operation }
+
+    it "has the right operation type" do
+      expect(operation.operation_type).to eq "play_card"
+    end
+
+    it "has the right card" do
+      expect(operation.card).to eq card
+    end
+  end
 
   describe "#card" do
     context "when the operation is passing a trump suit" do
       let(:operation) {
         Operation.create!(operation_type: :pass_trump,
-                       suit: :diamonds)
+                          suit: :diamonds)
       }
 
       it "should not have an associated card" do
