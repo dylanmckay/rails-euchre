c02acdd79b326c89c13b709b0fd3da2bd1707591
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-12-11 10:11:30 +1300

    Moved 'AdvanceGame' from 'controllers' to 'services'
    Whoopsies

--- app/services/advance_game.rb
+++ app/services/advance_game.rb
@@ -2,12 +2,10 @@ class AdvanceGame
   def initialize(game)
     @game = game
     @game_state = CreateGameState.new(@game).call
-
   end
 
   def call
-
-  while !is_user?(ply = NextPlayer.new(@game_state, @game).call)
+    while !is_user?(ply = NextPlayer.new(@game_state, @game).call)
       AI::DecideOperations.new(@game, @game_state, ply).call
       @game.operations(reload: true)
       @game_state = CreateGameState.new(@game).call
@@ -15,8 +13,6 @@ class AdvanceGame
   end
 
   private
-
-
 
   def is_user?(player)
     @game.main_player == player
