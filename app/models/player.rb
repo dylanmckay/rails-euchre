1d2ebb07e607f147ee6691e67ecad6cadc121f69
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-12-15 16:30:15 +1300

    Removed '==' method for Player model

--- app/models/player.rb
+++ app/models/player.rb
@@ -2,14 +2,4 @@ class Player < ActiveRecord::Base
   has_many :operations, dependent: :destroy
   belongs_to :game
   belongs_to :user
-
-  # TODO: don't mess with ActiveRecord operatprs
-  # player state stuff is bad too
-  def ==(other)
-    if other.is_a?(Player) || other.is_a?(PlayerState)
-      id == other.id
-    else
-      false
-    end
-  end
 end
