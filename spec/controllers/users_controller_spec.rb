af2af2e1afa153c5c9ebe7545c2ec8ebcf0461d3
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-12-16 14:34:36 +1300

    Added 'users' controller test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
+  render_views
 
+  describe "GET #index" do
+    it "is a success" do
+      get 'index'
+      expect(response).to be_success
+    end
+
+  end
 end
