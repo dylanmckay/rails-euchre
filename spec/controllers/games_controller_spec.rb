0ebc3257a39651820be43f4427269367c07c5cdb
Dylan McKay <dylan.mckay@powershop.co.nz>
2015-12-22 12:04:46 +1300

    Added controller tests

--- spec/controllers/games_controller_spec.rb
+++ spec/controllers/games_controller_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe GamesController, type: :controller do
-
+  render_views
+  #
+  # describe "#show" do
+  #   it "is a success" do
+  #     get :show, user: User.create!(name: "A"), game: Game.create!
+  #
+  #     expect(response).to be_success
+  #   end
+  # end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -3,11 +3,15 @@ require 'rails_helper'
 RSpec.describe UsersController, type: :controller do
   render_views
 
-  describe "GET #index" do
+  describe "#index" do
     it "is a success" do
-      get 'index'
+      get :index
       expect(response).to be_success
     end
 
+    it "renders the index template" do
+      get :index
+      expect(response).to render_template("index")
+    end
   end
 end
