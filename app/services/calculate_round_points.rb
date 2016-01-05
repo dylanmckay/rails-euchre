96d615011736fff9b3a9fc325ca1aec986672b30
TanaTanoi <tana.tanoi@gmail.com>
2016-01-06 09:52:15 +1300

    Cleaned up calculate_round_points

--- app/services/calculate_round_points.rb
+++ app/services/calculate_round_points.rb
@@ -1,30 +1,48 @@
-
 class CalculateRoundPoints
   def initialize(game_state)
     @game_state = game_state
   end
 
   def call
-    @game_state.players.map do |player|
-      player_points(player)
+    @game_state.players.map do |player_state|
+      player_state_points(player_state)
     end
   end
 
-  def player_points(player)
-    standard_points(player) + bonus_points(player)
+  private
+
+  def player_state_points(player_state)
+    standard_points(player_state) + bonus_points(player_state)
   end
 
-  def bonus_points(player)
-    euchred_other_player?(player) ? 2 : 0
+  def standard_points(player_state)
+    if player_won_trick_with_march?(player_state)
+      2
+    elsif player_won_trick?(player_state)
+      1
+    else
+      0
+    end
   end
 
-  #TODO rename 'player' to 'player_state' if they are states
-  def euchred_other_player?(player)
-    !player_picked_trump?(player) && won_trick_count_for_player(player) >= winning_trick_amount
+  def bonus_points(player_state)
+    euchred_other_player_state?(player_state) ? 2 : 0
   end
 
-  def player_picked_trump?(player_state)
-    player_state.player == @game_state.trump_state.trump_selector
+  def euchred_other_player_state?(player_state)
+    !player_state_picked_trump?(player_state) && player_won_trick?
+  end
+
+  def player_won_trick?(player_state)
+    won_trick_count_for_player(player_state) >= winning_trick_amount
+  end
+
+  def player_won_trick_with_march?(player_state)
+    won_trick_count_for_player(player_state) == winning_trick_march_amount
+  end
+
+  def player_state_picked_trump?(player_state_state)
+    player_state_state.player == @game_state.trump_state.trump_selector
   end
 
   def winning_trick_amount
@@ -35,17 +53,7 @@ class CalculateRoundPoints
     Config::HAND_CARD_COUNT
   end
 
-  def standard_points(player)
-    if won_trick_count_for_player(player) == winning_trick_march_amount
-      2
-    elsif won_trick_count_for_player(player) >= winning_trick_amount
-      1
-    else
-      0
-    end
-  end
-
-  def won_trick_count_for_player(player)
-    player.scored_cards.length / @game_state.players.length
+  def won_trick_count_for_player(player_state)
+    player_state.scored_cards.length / @game_state.player_states.length
   end
 end
