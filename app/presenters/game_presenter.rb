9c2af3332d88c8501392925a67e6e5f0ddd34b79
Dylan McKay <dylan.mckay@powershop.co.nz>
2016-01-06 15:54:08 +1300

    Merged GamesHelper and presenter

--- app/presenters/game_presenter.rb
+++ app/presenters/game_presenter.rb
@@ -5,6 +5,104 @@ class GamePresenter < SimpleDelegator
     operations.last(EVENT_LOG_ENTRIES).map {|op| description(op)}.join("\n")
   end
 
+  def points_string(point_count)
+    if point_count == 1
+      "#{point_count} Point"
+    else
+      "#{point_count} Points"
+    end
+  end
+
+  def hand_card_css_class(dynamic:)
+    if dynamic
+      "dynamic_hand_card"
+    else
+      "hand_card"
+    end
+  end
+
+  def unicode_card(card)
+    UNICODE_CARDS[card.suit][card.rank-1]
+  end
+
+  def unicode_suit(suit)
+    UNICODE_SUITS[suit]
+  end
+
+  def unicode_card_back
+    "🂠"
+  end
+
+  UNICODE_SUITS = {
+    spades: "♠",
+    hearts: "♥",
+    diamonds: "♦",
+    clubs: "♣",
+  }
+
+  UNICODE_CARDS = {
+    spades: [
+      "🂡",
+      "🂢",
+      "🂣",
+      "🂤",
+      "🂥",
+      "🂦",
+      "🂧",
+      "🂨",
+      "🂩",
+      "🂪",
+      "🂫",
+      "🂭",
+      "🂮"
+    ],
+    hearts: [
+      "🂱",
+      "🂲",
+      "🂳",
+      "🂴",
+      "🂵",
+      "🂶",
+      "🂷",
+      "🂸",
+      "🂹",
+      "🂺",
+      "🂻",
+      "🂽",
+      "🂾"
+    ],
+    diamonds: [
+      "🃁",
+      "🃂",
+      "🃃",
+      "🃄",
+      "🃅",
+      "🃆",
+      "🃇",
+      "🃈",
+      "🃉",
+      "🃊",
+      "🃋",
+      "🃍",
+      "🃎",
+    ],
+    clubs: [
+      "🃑",
+      "🃒",
+      "🃓",
+      "🃔",
+      "🃕",
+      "🃖",
+      "🃗",
+      "🃘",
+      "🃙",
+      "🃚",
+      "🃛",
+      "🃝",
+      "🃞",
+    ],
+  }
+
 private
 
   def description(operation)
