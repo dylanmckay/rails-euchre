d5afc5005ad5fd0779d498249385d19ff2e2688e
TanaTanoi <tana.tanoi@gmail.com>
2016-01-06 14:52:40 +1300

    Removed old url methods in favour of the route methods in the view
    -Changed url_for methods into new_game_player_operation_path methods

--- app/presenters/game_presenter.rb
+++ app/presenters/game_presenter.rb
@@ -5,38 +5,7 @@ class GamePresenter < SimpleDelegator
     operations.last(EVENT_LOG_ENTRIES).map {|op| description(op)}.join("\n")
   end
 
-  def card_link_url(card, operation, read_only: false)
-    if read_only
-      "javascript:void(0);"
-    else
-      operation_url(operation, card.suit, card.rank)
-    end
-  end
-
-  def pass_trump_operation_url
-    operation_url("pass_trump")
-  end
-
-  def accept_trump_operation_url
-    operation_url("accept_trump")
-  end
-  
-  private
-
-  #TODO in the wrong place
-  def operation_url(operation_type, suit=nil, rank=nil)
-    args = {}
-    args[:operation_type] = operation_type
-    args[:suit] = suit if suit
-    args[:rank] = rank if rank
-
-    new_operation_url + args.to_query
-  end
-
-  def new_operation_url
-    "/games/#{id}/players/#{players.first.id}/operations/new?"
-  end
-
+private
 
   def description(operation)
     player_name = operation.player.user.name
